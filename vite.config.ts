07e93fa713a6f81cc95b4d6e58c43d1d1fbaf92f
seanmars <mars0222@gmail.com>
2025-05-07 00:21:05 +0800

    feat: add build file

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        tailwindcss(),
-    ],
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
-    }
-})
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'docs',
+  }
+});
