626f195d725903b62fe58f6b412b88cf7f607508
seanmars <mars0222@gmail.com>
2025-05-08 12:03:29 +0800

    feat: change vue-route to use hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createMemoryHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '../components/Layout.vue'
 
 export const constantRoutes = [
@@ -31,7 +31,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
